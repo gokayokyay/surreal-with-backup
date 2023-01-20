00dbae6eb9722e8f4bd9006c15f801d05ef17764
Gökay Okyay <gokayokyay@hotmail.com>
2023-01-20 21:55:21 +0000

    feat(backup-man): add daemonize-me pkg

--- backup-man/src/main.rs
+++ backup-man/src/main.rs
@@ -1,3 +1,29 @@
+use std::any::Any;
+use std::fs::File;
+use std::process::exit;
+
+pub use daemonize_me::Daemon;
+
 fn main() {
-    println!("Hello, world!");
+    let stdout = File::create("info.log").unwrap();
+    let stderr = File::create("err.log").unwrap();
+    let daemon = Daemon::new()
+        .pid_file("backman.pid", Some(false))
+        .umask(0o000)
+        .work_dir(".")
+        .stdout(stdout)
+        .stderr(stderr)
+        .start();
+
+    match daemon {
+        Ok(_) => println!("Daemonized with success"),
+        Err(e) => {
+            eprintln!("Error, {}", e);
+            exit(-1);
+        },
+    }
+
+    for i in 0..=10000 {
+        println!("{}", i);
+    }
 }
