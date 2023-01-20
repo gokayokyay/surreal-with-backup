1c33e10c6f3abfd7298442c7cb5627d2fcdd2f59
Gökay Okyay <gokayokyay@hotmail.com>
2023-01-20 21:54:42 +0000

    build(surreal): improve script?

--- build_surreal.sh
+++ build_surreal.sh
@@ -1,1 +1,15 @@
-(cd surrealdb && make build)
+echo "WARNING!"
+echo "This script is meant for vms or containers"
+echo "Please just install surrealdb official way instead of compiling it..."
+
+rm build_out.log 2> /dev/null
+
+# https://github.com/soedinglab/hh-suite/issues/280#issuecomment-1076146671
+sudo mkdir -p /var/cache/swap/
+sudo fallocate -l 16G /var/cache/swap/swapfile
+sudo chmod 600 /var/cache/swap/swapfile
+sudo mkswap /var/cache/swap/swapfile
+
+(cd surrealdb && make build >> ../build_out.log 2>&1)
+
+sudo swapoff --all
